142d493386dc5e0830c2d8e5dde9e24cd9c8a9e2
opilane <Kregorlatt1221@gmail.com>
2022-11-22 15:22:57 +0200

    Add Book model

---
+++ Latt_Library/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace Latt_Library.Models
+{
+    public class Book
+    {   // 1, Tõde ja Õigus, A.H.Tammsaare, 01.11.21, 15.11.21, available
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Author { get; set; }
+        public DateTime RentalDate { get; set; }
+        public DateTime RentalLenght { get; set; }
+        public Boolean IsAvailable { get; set; }
+    }
+
+}
